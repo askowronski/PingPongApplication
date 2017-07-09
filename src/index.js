453bc4dd51100201091bd171c2d4027e7f1c671c
askowronski <askowronski@backstopsolutions.com>
2017-07-09 18:56:31 -0500

    Adding npm config and front end packaging

---
+++ src/index.js
@@ -0,0 +1,136 @@
+const React = require('react');
+const ReactDOM = require('react-dom')
+const jQuery = require('jquery')
+
+const Button = (props) => {
+    return (
+      <button onClick={()=> props.getGamesPlayed(2)}> Get Player </button>
+    );
+}
+
+const Display = (props) => {
+    return (
+        <div>
+      <span>Games Played</span>
+            <span> equals + {props.gamesPlayed} </span>
+        </div>
+
+    );
+}
+
+const input = (props) => {
+    return (
+        <input id="idInput" type='submit' >input</input>
+    );
+}
+
+class App extends React.Component {
+    state = {
+        gamesPlayed:0,
+        averageScore: 0,
+    };
+
+    getGamesPlayed = (id) => {
+
+        jQuery.ajax({
+
+            url: "http://localhost:8080/GetPlayer?id="+id,
+            dataType: 'json',
+            cache: false,
+            error: function (xhr, status, err) {
+                console.error("http://localhost:8080/GetPlayer?id="+id, status, err.toString());
+            }
+        }).then(function (data) {
+            this.setState({
+                gamesPlayed: JSON.parse(data.message).username
+            })
+        }.bind(this));
+    };
+
+    render() {
+        const {gamesPlayed} = this.state;
+
+        return(
+           <div>
+               {/*<Button getGamesPlayed={this.getGamesPlayed} gamesPlayed = {gamesPlayed} />*/}
+               {/*<Display gamesPlayed={gamesPlayed}/>*/}
+               {/*<input />*/}
+               {/*<form id="idInputForm" action={}>*/}
+                   {/*Player ID: <input type="text" name="id" /><br/>*/}
+                   {/*<input type="submit" value="Submit"  />*/}
+               {/*</form>*/}
+               <IDForm />
+               {/*<Display gamesPlayed={gamesPlayed} />*/}
+           </div>
+        );
+    }
+}
+
+class IDForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            gamesPlayed:0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    getGamesPlayed = (id) => {
+
+        jQuery.ajax({
+
+            url: "http://localhost:8080/GetPlayer?id="+id,
+            dataType: 'json',
+            cache: false,
+            error: function (xhr, status, err) {
+                console.error("http://localhost:8080/GetPlayer?id="+id, status, err.toString());
+            }
+        }).then(function (data) {
+            window.alert(data.message);
+            this.setState({
+
+                gamesPlayed: JSON.parse(data.message).username
+            })
+            if(this.gamesPlayed===null){
+                this.setState({
+                    gamesPlayed: "User Not Found"
+                })
+
+            }
+        }.bind(this));
+    };
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        {this.getGamesPlayed(this.state.value)};
+    }
+
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+
+            <Display gamesPlayed={this.state.gamesPlayed} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
+
--- src/main/java/app/API/PlayerAPI.java
+++ src/main/java/app/API/PlayerAPI.java
@@ -3,6 +3,7 @@ package app.API;
 import app.StatsEngine.EloRating;
 import app.PersistenceManagers.PlayerPersistenceManager;
 import app.StatsEngine.Player;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,8 @@ public class PlayerAPI {
 
     }
 
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/GetPlayer")
     public APIResult processGetPlayer(@RequestParam(value="id", required=true) int id) {
         PlayerPersistenceManager pPM = new PlayerPersistenceManager();
