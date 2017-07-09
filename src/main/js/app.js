58add63e848367f9fe6d8b728cf4a0e70094e71d
askowronski <askowronski@backstopsolutions.com>
2017-07-09 11:22:45 -0500

    Prepare to setup react

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,11 +2,3 @@ const React = require('react');
 const ReactDOM = require('react-dom')
 const client = require('./client');
 
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {employees: []};
-    }
-
-}
